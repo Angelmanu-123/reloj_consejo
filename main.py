6389d66557d9fb6b643b4f3ff5cfd5257288475a
ixchop7 <estudiantev@COED.LOCAL>
2025-09-26 02:11:42 -0600

    nnuevos cambios

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print(" cris ")
+from tkinter import *
+import requests
+def actualizar():
+    url ="https://timeapi.io/api/time/current/zone?timeZone=America%2FGuatemala"
+
+    data = requests.get(url).json()
+    anio = data["year"]
+    hora = data["hour"]
+    minutos =data["minute"]
+    segundos = data["seconds"]
+
+
+    texto = (f"hora: {hora}  " 
+             f" minutos: {minutos}  " 
+             f" segundos: {segundos}  " 
+             f"anio :{anio}")
+    etiqueta.config(text=texto)
+
+
+    app.after(1000, actualizar)
+
+app =Tk()
+
+app.title("aplicacion de la hora")
+app.geometry("300x200")
+app.resizable(width=False,height=False)
+
+etiqueta = Label(app,text = "hora",fg="blue")
+etiqueta.grid(column =0,row=1,padx=3,pady=3)
+
+actualizar()
+app.mainloop()
